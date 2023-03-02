7ea73e1d49191149596c6e2ea782d0857c673363
Bryann Henrique <bryann.enrique@gmail.com>
2023-03-02 16:42:28 -0300

    Criação e login finalizados.

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,7 @@
-import { PrismaClient } from '@prisma/client';
 import express from 'express';
-import { Router, Request, Response } from 'express';
+import route from './endpoints';
 
 const app = express();
-const route = Router();
-
-route.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello world!',
-  });
-});
-
-const prisma = new PrismaClient();
-
-route.get('/user/all', async (req: Request, res: Response) => {
-  const users = await prisma.users.findMany();
-
-  res.send(users);
-});
-
-route.get('/user/create', async (req: Request, res: Response) => {
-  const name = req.query.name as string;
-  const response = await prisma.users.create({
-    data: {
-      name
-    }
-  });
-
-  res.status(201).send(response);
-});
 
 app.use(express.json());
 app.use(route);
