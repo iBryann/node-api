2c5b23b07d8cc381755389f808c147dbef439d00
Bryann Henrique <bryann.enrique@gmail.com>
2023-03-03 19:16:03 -0300

    Iniciando implementação de autenticação de rotas

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import { listRouter, taskRouter, userRouter } from './endpoints';
 import express from 'express';
+import { routes } from './endpoints';
 
 const app = express();
 
 app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
-app.use(listRouter);
+routes.forEach(route => app.use(route));
 app.listen(3333, () => 'server running on port 3333 🚀');
